71d82853f2f06ba7c4b798fb6acaf2eac0542cf8
Chris Kiriakou <chris-kiriakou@web.de>
2024-08-24 11:15:55 +0200

    Added installation of required software.

--- setup.sh
+++ setup.sh
@@ -1,3 +1,21 @@
 #!/bin/sh
 
-echo "Hello There!"
+set -eu
+
+LOCATION=$(pwd)
+
+sudo pacman -S \
+  python3 \
+  ansible \
+  git
+
+git clone https://github.com/ckiri/arch-playbook
+cd playbook
+
+ansible-playbook -i inventory -K playbook.yml
+
+cd $LOCATION
+
+rm -r arch-playbook
+
+exit 0
