700d633f162e1a05bc7b2b4bc7414409445a7995
Chris Kiriakou <chris-kiriakou@web.de>
2024-08-25 19:02:52 +0200

    Added new packages.

--- setup.sh
+++ setup.sh
@@ -1,23 +1,39 @@
 #!/bin/sh
+# This script installs a minimal but usable Arch-Linux setup
 
-main() {
-  LOCATION=$(pwd)
+DIR="/home/${USER}/"
+REQ_PACKAGES="python3 ansible git"
 
-  sudo pacman -Syu \
-    python3 \
-    ansible \
-    git
+cd $DIR
 
-  git clone https://github.com/ckiri/arch-playbook
-  cd arch-playbook
+# Check for CPU vendor & install microcode update image
+printf "Detect CPU vendor and install res.\n"
+cpu=$(lscpu)
+[ -z $(grep -e "AMD" <<< $cpu) ] \
+  && REQ_PACKAGES="${REQ_PACKAGES} amd-ucode"
+[ -z $(grep -e "Intel" <<< $cpu) ] \
+  && REQ_PACKAGES="${REQ_PACKAGES} intel-ucode"
 
-  ansible-playbook -i inventory -K playbook.yml
+# Check for GPU vendor & install drivers
+pci=$(lspci)
+printf "Detect GPU vendor and intall driver.\n"
+[ -z $(grep -E ".*VGA compatible controller:\s*[^:]+Radeon.*" <<< $pci) ] \
+  && REQ_PACKAGES="${REQ_PACKAGES} vulkan-radeon lib32-vulkan-radeon"
+[ -z $(grep -E ".*VGA compatible controller:\s*[^:]+NVIDIA.*" <<< $pci) ] \
+  && REQ_PACKAGES="${REQ_PACKAGES} nvidia lib32-nvidia-utils egl-wayland"
+[ -z $(grep -E ".*VGA compatible controller:\s*[^:]+Intel.*" <<< $pci) ] \
+  && REQ_PACKAGES="${REQ_PACKAGES} vulkan-intel lib32-vulkan-intel"
 
-  cd $LOCATION
+sudo pacman -Syu --needed $REQ_PACKAGES
 
-  rm -r arch-playbook
+# Run the ansible playbook
+git clone --depth=1 https://github.com/ckiri/arch-playbook
+cd ./arch-playbook
+printf "Running the Ansible Playbook.\n"
+ansible-playbook -i inventory -K playbook.yml
 
-  exit 0
-}
+# Cleanup
+cd $DIR
+rm -r ./arch-playbook
 
-main
+exit 0
