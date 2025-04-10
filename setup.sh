58192baee0376bd4e00af75f4609a0fa0946c160
Chris Kiriakou <chris-kiriakou@web.de>
2025-04-10 20:11:53 +0200

    Remove CPU & GPU vendor checks.

--- setup.sh
+++ setup.sh
@@ -8,25 +8,8 @@ main() {
     
     cd $DIR
     
-    # Check for CPU vendor & add microcode update image to the list
-    printf "Detect CPU vendor for installation of microcode update image.\n"
-    cpu=$(lscpu)
-    test -n "$(grep -e "AMD" < $cpu)" \
-        && REQ_PACKAGES="${REQ_PACKAGES} amd-ucode"
-    test -n "$(grep -e "Intel" < $cpu)" \
-        && REQ_PACKAGES="${REQ_PACKAGES} intel-ucode"
-    
-    # Check for GPU vendor & install drivers
-    pci=$(lspci)
-    printf "Detect GPU vendor for installation of driver.\n"
-    test -n "$(grep -E ".*VGA compatible controller:\s*[^:]+Radeon.*" < $pci)" \
-        && REQ_PACKAGES="${REQ_PACKAGES} vulkan-radeon lib32-vulkan-radeon"
-    test -n "$(grep -E ".*VGA compatible controller:\s*[^:]+NVIDIA.*" < $pci)" \
-        && REQ_PACKAGES="${REQ_PACKAGES} nvidia lib32-nvidia-utils egl-wayland"
-    test -n "$(grep -E ".*VGA compatible controller:\s*[^:]+Intel.*" < $pci)" \
-        && REQ_PACKAGES="${REQ_PACKAGES} vulkan-intel lib32-vulkan-intel"
-    
-    sudo pacman -Syu --confirm $REQ_PACKAGES
+    # Install required packages 
+    sudo pacman -Syu $REQ_PACKAGES
 
     # Run the ansible playbook
     git clone --depth=1 https://github.com/ckiri/arch-playbook
