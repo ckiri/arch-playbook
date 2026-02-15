2c6f50474f9b8724b1ddff7965a872f2de94f805
Chris Kiriakou <chris-kiriakou@web.de>
2026-02-16 00:13:26 +0100

    Update packages and add ufw rules.

--- setup.sh
+++ setup.sh
@@ -1,28 +1,23 @@
 #!/bin/sh
 #
-# This script installs a minimal but usable Arch-Linux setup
+# Install required software and start the playbook
 
-main() {
-    DIR="/home/${USER}/"
-    REQ_PACKAGES="archlinux-keyring python3 ansible git"
-    
-    cd $DIR
-    
-    # Install required packages 
-    sudo pacman -Syu $REQ_PACKAGES
-    test $? -ne 0 && exit 1
+DIR="/home/${USER}/"
+REQ_PACKAGES="archlinux-keyring ansible git"
 
-    # Run the ansible playbook
-    git clone --depth=1 https://github.com/ckiri/arch-playbook
-    cd ./arch-playbook
-    printf "Running the Ansible Playbook.\n"
-    ansible-playbook -i inventory -K playbook.yml
-    test $? -ne 0 && exit 1
+cd $DIR
 
-    # Cleanup
-    cd $DIR
-    sudo rm -r ./arch-playbook
-}
+# Install required packages 
+sudo pacman -Syu $REQ_PACKAGES
+test $? -ne 0 && exit 1
 
-main
-exit 0
+# Run the ansible playbook
+git clone --depth=1 https://github.com/ckiri/arch-playbook
+cd ./arch-playbook
+printf "Running the Ansible Playbook.\n"
+ansible-playbook -i inventory -K playbook.yml
+test $? -ne 0 && exit 1
+
+# Cleanup
+cd $DIR
+sudo rm -r ./arch-playbook
